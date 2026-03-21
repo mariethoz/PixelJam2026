72bae56018c84fa1b5d134f09e3545cdb4c50fab
wgoni <raphael.gonin@hes-so.ch>
2026-03-21 14:29:24 +0100

    Added first monster

---
+++ PixelJam2026/Characters/Ennemies/enemy.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var speed = 200
+var player: Node2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
